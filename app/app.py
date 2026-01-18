8045dc8b7f0b7af30da099682011963e5db95360
Amsatnaf <45526389+Amsatnaf@users.noreply.github.com>
2026-01-17 22:54:18 -0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -9,14 +9,9 @@ metrics.info('app_info', 'Application info', version='1.0.3')
 
 @app.route('/')
 def hello():
-<<<<<<< HEAD
-   ##return 'Olá, mundo! 👋', 200
-   return 'Olá, CI - CD com Rancher Fleet e GitHub! 🚀', 200
-=======
   ##return 'Olá, mundo! 👋', 200
     return 'Olá, CI - CD com Rancher Fleet e GitHub - Coloquei metricas de monitoramento! 🚀', 200
 
->>>>>>> nova-mensagem
 if __name__ == '__main__':
     # Importante: host 0.0.0.0 para funcionar no Docker
     app.run(host='0.0.0.0', port=8080)
